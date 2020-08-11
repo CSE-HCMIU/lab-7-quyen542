ef4a44298f96a1197ce8516dd8d3ff3ef1cf256a
Quyen Duong <63244142+quyen542@users.noreply.github.com>
2020-08-11 10:06:40 +0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -14,9 +14,75 @@ Ex:
 #include<string.h>
 #include<limits.h>
 #define SIZE 1000
+void number(int *a, int *b, char c[])
+{
+	int a1,b1;
+	a1=b1=0;
 
+	int sa = 0, sb = 0, i = 0;
+	if (c[0] == '-')
+	{
+		sa = 1;
+		i++;
+	}
+	while (c[i] >= 48 && c[i] <= 57)
+	{
+		a1 = a1* 10 + (c[i] - '0');
+		i++;
+	}
+	if (c[i] == '-')
+	{
+		sb = 1;
+		i++;
+	}
+	else if (c[i] == '+')
+	{
+		sb = 0;
+		i++;
+	}
+	while (c[i] >= 48 && c[i] <= 57)
+	{
+		if (c[i] == 'i')
+		{
+			break;
+		}
+		b1 = b1 * 10 + (c[i] - '0');
+		i++;
+	}
+	if (sa == 1)
+	{
+		a1 = a1 * (-1);
+	}
+	if (sb == 1)
+	{
+		b1 = b1 * (-1);
+	}
+	*a=a1;
+	*b=b1;
+}
 void ex3(char s1[], char s2[]){
-	//your codes here
+	char  result[100], temp1[100], temp2[100];
+	int a, b, a1, b1;
+	number(&a, &b, s1);
+	number(&a1, &b1, s2);
+	int m = a * a1 - b * b1;
+	int n = a * b1 + a1 * b;
+	itoa(m,temp1,10);
+	itoa(n,temp2,10);
+	strcpy(result,temp1);
+	if (n > 0)
+	{
+		result[strlen(result)]='+';
+		strcat(result,temp2);
+		result[strlen(result)]='i';
+	}
+	else
+	{
+		strcat(result,temp2);
+		result[strlen(result)]='i';
+
+	}
+	puts(result);
 }
 
 int main(int argc, char *argv[]) {
