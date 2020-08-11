e47464cda98d98263689d203bf85aef2e51518ac
Quyen Duong <63244142+quyen542@users.noreply.github.com>
2020-08-11 10:23:22 +0700

    Update Exercise4.c

--- Exercise4.c
+++ Exercise4.c
@@ -14,8 +14,39 @@ Ex:
 #include<limits.h>
 #define SIZE 1000
 
+int b2d(int n)
+{
+	int dec = 0;
+	int i = 0;
+	while (n > 0)
+	{
+		dec = dec + (n % 10) * pow(2, i);
+		i++;
+		n = n / 10;
+	}
+	return dec;
+}
+int d2b(int n)
+{
+	int bin = 0;
+	int i = 0;
+	while (n > 0)
+	{
+		bin = bin + (n % 2) * pow(10, i);
+		i++;
+		n = n / 2;
+	}
+	return bin;
+}
 void ex4(char s1[], char s2[]){
-	//your codes here
+    int result;
+	int num1 = atol(s1);
+	int num2 = atol(s2);
+	int a = b2d(num1);
+	int b = b2d(num2);
+	int c = a + b;
+	result = d2b(c);
+	printf("%d",result);
 }
 
 int main(int argc, char *argv[]) {
