2210d9313f3c645d8ba6fa8d0ca9c2d48c3441e0
Quyen Duong <63244142+quyen542@users.noreply.github.com>
2020-08-11 08:17:14 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -13,8 +13,39 @@
 #include<limits.h>
 #define SIZE 1000
 
+void sort(char a[100][100], int n)
+{
+	char temp[100];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (strcmp(a[i], a[j]) > 0)
+			{
+				strcpy(temp, a[i]);
+				strcpy(a[i], a[j]);
+				strcpy(a[j], temp);
+			}
+		}
+        }
+}
+
 void ex1(char *str[],int n){
-	//your codes here
+		char result[100];
+		sort(str, n);
+		for (int i = 0; i < strlen(str[0]); i++)
+		{
+			if (str[0][i] == str[n - 1][i])
+			{
+				result[i] = str[0][i];
+			}
+			else
+			{
+				break;
+			}
+		}
+	puts(result);
+	
 }
 
 int main(int argc, char *argv[]) {
